a40ec654d836fb8d44140258438bfbec37e7cfe7
chuechang <chuechang.cc@gmail.com>
2021-06-23 20:51:45 -0700

    corrected script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/* Print "Programming is like building a multilingual puzzle
+ * using puts
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	getchar();
 	return (0);
 }
