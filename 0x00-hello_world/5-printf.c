b435de2caad07fa483fcac8a09533289b4ff0cba
chuechang <chuechang.cc@gmail.com>
2021-06-23 21:26:02 -0700

    added correct file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - use printf to display sentences
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
