bd7c807575c75d9682ae65c724003f27aa21a2a3
chuechang <chuechang.cc@gmail.com>
2021-06-25 14:21:50 -0700

    added task 4 code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - print aplhabets in lowercase
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	if (x != 'q' && x != 'e')
+	{
+	putchar(x);
+	}
+	putchar ('\n');
+	return (0);
+}
