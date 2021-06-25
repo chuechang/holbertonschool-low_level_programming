902c5de35b254d9b04309c93250068973feb2573
chuechang <chuechang.cc@gmail.com>
2021-06-25 16:57:08 -0700

    added task 7 code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Prints lowercase letters in reverse
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; --x)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
