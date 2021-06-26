7ccbc6820b508b91389c212279b138b72e84c7bd
chuechang <chuechang.cc@gmail.com>
2021-06-25 17:57:25 -0700

    added task 9 code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Prints all possible combinations of single-digit numbers
+  *
+  * Retunn: 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
