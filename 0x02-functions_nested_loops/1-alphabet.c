a007514d8723ca139ee47857d2bb7ec3fe8c0aec
chuechang <chuechang.cc@gmail.com>
2021-06-28 14:51:16 -0700

    added task 1 and update holberton header

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * print_alphabet - Print the alphabets in lowercase
+  *
+  * Return: 0
+  */
+
+void print_alphabet(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,7 +1,2 @@
-#ifndef HOLBERTON_DOT_H
-#define HOLBERTON_DOT_H
-
 int _putchar(char c);
-
-
-#endif
+void print_alphabet(void);
