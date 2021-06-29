43db7f11f02743ad7f62a2e468b5195459aa68d4
chuechang <chuechang.cc@gmail.com>
2021-06-28 19:44:19 -0700

    updated holberton header and added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * add - add two integers
+  * @num1: integer 1
+  * @num2: intger 2
+  *
+  * Return: sum of num1 and num2
+  */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
