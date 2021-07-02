6847f44a82ae6b7b943f209a37d8ac95cd41d277
chuechang <chuechang.cc@gmail.com>
2021-07-01 18:45:58 -0700

    added task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+  * more_numbers - print from 0-14 ten times
+  *
+  * Use nested loop to print
+  *
+  */
+void more_numbers(void)
+{
+	char i;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
