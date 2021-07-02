0501de411cbade70a1f9a5df20cef0d0de3cf4c3
chuechang <chuechang.cc@gmail.com>
2021-07-01 18:37:53 -0700

    added task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  * print_most_numbers - Print from 0-9 but not 2 and 4
+  *
+  * Return: 0
+  */
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
+
