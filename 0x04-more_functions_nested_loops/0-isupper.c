eb8903090521741af1aa218c67047fff19997793
chuechang <chuechang.cc@gmail.com>
2021-07-01 17:46:29 -0700

    added task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * _isupper - checks for uppercase character
+  * 
+  * @c: character to be checked
+  * Return: 1 if c is uppercase letter, returns 0 if otherwise
+  */
+int _isupper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else 
+		return (0);
+}
