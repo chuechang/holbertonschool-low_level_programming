bbe80913b7043dd965bbdfd4ee11fa434e5f82af
chuechang <chuechang.cc@gmail.com>
2021-07-01 18:08:57 -0700

    added task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  * mul - multiples two integers
+  * @a: first integer
+  * @b: second intger
+  *
+  * Return: product of a*b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
