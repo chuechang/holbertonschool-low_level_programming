65d7dd6722cd933184f293cc8b83029082590cd6
chuechang <chuechang.cc@gmail.com>
2021-07-01 18:27:35 -0700

    added task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * print_numbers - Print from 0 - 9
+  *
+  */
+void print_numbers(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
